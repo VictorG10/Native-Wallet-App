ad9a3979797f2306f80032731003d8d92ae9d6f6
VictorG10 <victorgbadebo10@gmail.com>
2025-07-25 20:21:58 +0100

    adding transaction routes endpoint

--- backend/src/server.js
+++ backend/src/server.js
@@ -29,6 +29,53 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(logger);
 
+app.get("/api/transactions/:userId", async (req, res) => {
+  try {
+    const { userId } = req.params;
+
+    const transactions = await sql`
+    SELECT * FROM transactions WHERE user_id = ${userId} ORDER BY created_at DESC
+    `;
+    console.log(transactions);
+    res.status(200).json(transactions);
+  } catch (error) {
+    console.error("Error fetching transaction:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
+
+app.get("/api/transactions/summary/:userId", async (req, res) => {
+  try {
+    const { userId } = req.params;
+
+    const balanceResult = await sql`
+    SELECT COALESCE(SUM(amount), 0) as balance FROM transactions WHERE user_id = ${userId}
+    `;
+
+    const incomeResult = await sql`
+    SELECT COALESCE(SUM(amount), 0) as income FROM transactions WHERE user_id = ${userId} AND amount > 0
+    `;
+
+    const expenseResult = await sql`
+    SELECT COALESCE(SUM(amount), 0) as expenses FROM transactions WHERE user_id = ${userId} AND amount < 0
+    `;
+
+    console.log({
+      balance: balanceResult[0].balance,
+      income: incomeResult[0].income,
+      expenses: expenseResult[0].expenses,
+    });
+    res.status(200).json({
+      balance: balanceResult[0].balance,
+      income: incomeResult[0].income,
+      expenses: expenseResult[0].expenses,
+    });
+  } catch (error) {
+    console.error("Error getting the summary", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
+
 app.post("/api/transactions", async (req, res) => {
   try {
     const { title, amount, category, user_id } = req.body;
@@ -47,6 +94,28 @@ app.post("/api/transactions", async (req, res) => {
     res.status(201).json(transaction[0]);
   } catch (error) {
     console.error("Error creating transaction", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
+
+app.delete("/api/transactions/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    if (isNaN(parseInt(id))) {
+      return res.status(400).json({ message: "Invalid transaction Id" });
+    }
+
+    const transactions = await sql`
+    DELETE FROM transactions WHERE id = ${id} RETURNING * `;
+
+    if (transactions.length === 0) {
+      return res.status(404).json({ message: "Transaction not found" });
+    }
+
+    res.status(200).json({ message: "Transaction deleted successfully" });
+  } catch (error) {
+    console.error("Error deleting transaction", error);
     res.status(500).json({ message: "Internal server error" });
   }
 });
