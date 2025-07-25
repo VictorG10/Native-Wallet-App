ff5ee22cb0e5390b31a572e850c3687f2f6c4c61
VictorG10 <victorgbadebo10@gmail.com>
2025-07-25 19:15:02 +0100

    creating transaction api

--- backend/src/server.js
+++ backend/src/server.js
@@ -24,12 +24,31 @@ const initDB = async () => {
   }
 };
 
+// middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(logger);
 
-app.get("/", (req, res) => {
-  res.send("<h1>Hello World</h1>");
+app.post("/api/transactions", async (req, res) => {
+  try {
+    const { title, amount, category, user_id } = req.body;
+
+    if (!title || !user_id || amount === undefined || !category) {
+      return res.status(404).json({ message: "All fields are required" });
+    }
+
+    const transaction = await sql`
+    INSERT INTO transactions(user_id, title, amount,category)
+    VALUEs (${user_id}, ${title}, ${amount}, ${category})
+    RETURNING *
+    `;
+
+    console.log(transaction);
+    res.status(201).json(transaction[0]);
+  } catch (error) {
+    console.error("Error creating transaction", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
 });
 
 initDB().then(() => {
